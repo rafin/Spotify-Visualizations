6d3a7dbe30504c5b525dabd4c244043443178e31
rafin <ryanafinley@gmail.com>
2016-06-30 08:58:59 -0500

    more heroku 2

--- spot/keys.py
+++ spot/keys.py
@@ -8,13 +8,13 @@ uir = "http://localhost:8888"
 username = "rino21111"
 
 spotify_keys = os.environ["SPOTIFY_KEYS"]
-print "IN KEYS.PY"
-print spotify_keys
 spotify_keys = ast.literal_eval(spotify_keys)
 
 
 #set up access
 def get_access():
+  print "IN KEYS.PY"
+  print spotify_keys
   for key in spotify_keys:
     try:
       token = util.prompt_for_user_token(username, scope, key["uid"], key["usec"], uir)
