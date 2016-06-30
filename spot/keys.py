98223621bfb350f1081e1e201cdfaef24cdfe1b3
rafin <ryanafinley@gmail.com>
2016-06-30 00:39:47 -0500

    more heroku tests

--- spot/keys.py
+++ spot/keys.py
@@ -8,6 +8,7 @@ uir = "http://localhost:8888"
 username = "rino21111"
 
 spotify_keys = os.environ["SPOTIFY_KEYS"]
+print "IN KEYS.PY"
 print spotify_keys
 spotify_keys = ast.literal_eval(spotify_keys)
 
@@ -17,6 +18,8 @@ def get_access():
   for key in spotify_keys:
     try:
       token = util.prompt_for_user_token(username, scope, key["uid"], key["usec"], uir)
+      print "SUCCESS"
       return spotipy.Spotify(auth=token)
     except:
+      print "FAILED TO LOAD"
       continue
