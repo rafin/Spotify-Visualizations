44d73c0873e60fde2557263dbb72f4045e5573d4
rafin <ryanafinley@gmail.com>
2016-06-30 09:34:16 -0500

    changed key format

--- spot/keys.py
+++ spot/keys.py
@@ -7,27 +7,21 @@ scope = "playlist-read-private"
 uir = "http://localhost:8888"
 username = "rino21111"
 
-spotify_keys = os.environ["SPOTIFY_KEYS"]
-spotify_keys = ast.literal_eval(spotify_keys)
+spotify_uid = os.environ["SPOTIFY_UID"]
+spotify_usec = os.environ["SPOTIFY_USEC"]
 print "IN KEYS.PY"
-print spotify_keys
-print spotify_keys[0]["uid"]
-print spotify_keys[0]["usec"]
+print spotify_uid
+print spotify_usec
+
 
 
 #set up access
 def get_access():
-  print "IN KEYS.PY"
-  print spotify_keys
-  print spotify_keys[0]["uid"]
-  print spotify_keys[0]["usec"]
-  for key in spotify_keys:
     try:
-      token = util.prompt_for_user_token(username, scope, key["uid"], key["usec"], uir)
+      token = util.prompt_for_user_token(username, scope, spotify_uid, spotify_usec, uir)
       print "SUCCESS"
       return spotipy.Spotify(auth=token)
     except:
       print "FAILED TO LOAD"
-      continue
 
 print get_access()
